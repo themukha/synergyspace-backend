d42b85c9b4618addf998e1048ff295ef0d9f632f
George Mukha <georgymukha@yandex.ru>
2024-12-14 23:19:49 +0500

    Added base configs to start up app

--- src/main/kotlin/io/synergyspace/Application.kt
+++ src/main/kotlin/io/synergyspace/Application.kt
@@ -1,11 +1,33 @@
 package io.synergyspace
 
+import io.ktor.http.ContentType
+import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.*
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
+import kotlinx.serialization.json.Json
 
 fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
 }
 
 fun Application.module() {
-    configureRouting()
+
+    install(ContentNegotiation) {
+        json(Json {
+            prettyPrint = true
+            isLenient = true
+        })
+    }
+
+    routing {
+        get("/") {
+            call.respondText("Welcome to SynergySpace", contentType = ContentType.Text.Plain)
+        }
+    }
 }
