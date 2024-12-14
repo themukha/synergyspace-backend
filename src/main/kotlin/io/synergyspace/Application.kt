a46e37db069fea27fcb42d7d90aedafb6805d97f
George Mukha <georgymukha@yandex.ru>
2024-12-14 22:00:02 +0500

    Git init

---
+++ src/main/kotlin/io/synergyspace/Application.kt
@@ -0,0 +1,11 @@
+package io.synergyspace
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}
